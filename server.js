298fe4b35a014512fe814df795a1310dce9cbd7d
Barapatre, Hemant <hbarapat@akamai.com>
2018-02-28 14:53:50 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname+'/views/partials')
+hbs.registerHelper('getCurrYear',()=>{
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt',(text)=>{
+    return text.toUpperCase();
+});
+
+
+app.set('view engine','hbs');
+
+app.use(express.static(__dirname+'/public'));
+app.use(express.static(__dirname+'/media'));
+
+//middle ware custom
+app.use((req ,res ,next)=> {
+var now =new Date().toString();
+var log = `${now} : ${req.method}; path:${req.url}`;
+fs.appendFileSync('server.log',log+'\n');
+
+var  pageMeta = {
+    pageTitle : 'Maintainace Page',
+    message : 'This Page is under maintaince'
+  };
+
+res.render('maint.hbs',pageMeta);
+//next();
+});
+
+
+app.get('/',(req,res)=>{
+    var pageMeta = {
+        PageTitle:'Home' ,
+        Content:'Hello',
+        media: __dirname+'/media/movie.mp4'
+    };
+res.render('index.hbs',pageMeta);
+});
+app.get('/about',(req,res)=>{
+  var  pageMeta = {
+      pageTitle : 'About Page',
+      message : 'La La La Lalalaalal..... Land'
+    };
+    res.render('about.hbs',pageMeta);
+});
+
+app.get('/bad',(req,res)=>{
+    res.send('<h1>Opss  Baby hit me On more time :)</h1>');
+});
+
+app.get('/maint',(req, res)=>{
+    var  pageMeta = {
+        pageTitle : 'Maintainace Page',
+        message : 'This Page is under maintaince'
+      };
+
+   res.render('maint.hbs',pageMeta);
+});
+   
+app.listen(3000);
